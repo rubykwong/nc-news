149a8e02cd01cc2708ae34ebfb197e0c87dc7ec6
Ruby Kwong <ruby.kwong@btinternet.com>
2025-06-09 17:02:19 +0100

    fix database connection and use dynamic port for render compatability

--- listen.js
+++ listen.js
@@ -1,9 +1,9 @@
 const app = require("./app")
-
-app.listen(8080, (err) => {
+const { PORT = 9090 } = process.env 
+app.listen(PORT, (err) => {
     if (err){
         console.log(err)
     } else {
-        console.log("listening on 8080")
+        console.log(`listening on ${PORT}`)
     }
 })
